35ca173ddef6e5150b121eb754fbb4d134b81471
Hamza Rahman <hamza.rahman2016@outlook.com>
2023-01-11 04:37:31 +0500

    ---

--- TestImageApp/Controllers/ImageController.cs
+++ TestImageApp/Controllers/ImageController.cs
@@ -28,20 +28,6 @@ namespace TestImageApp.Controllers
                 file.CopyTo(ms);
                 var fileBytes = ms.ToArray();
                 var id = Guid.NewGuid().ToString();
-                //string directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, DateTime.Now.ToString());
-                //if (!Directory.Exists(directory))
-                //{
-                //    Directory.CreateDirectory(directory);
-                //}
-                //string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,file.FileName);
-                //using (Stream filee = System.IO.File.Open(filePath,FileMode.OpenOrCreate)) 
-                //{
-                //    filee.Write(fileBytes, 0, fileBytes.Length);
-                //}
-                //ms.WriteTo
-                
-                //string s = Convert.ToBase64String(fileBytes);
-                // act on the Base64 data
                 _imageRepository.AddImage(new Image {Id=Guid.NewGuid().ToString(),ImageData= fileBytes, Name= Files.FirstOrDefault().Name });
             }
             
@@ -51,18 +37,7 @@ namespace TestImageApp.Controllers
         [Route("GetImages")]
         public List<Image> GetImages()
         {
-            //var files = new List<Image>();
             var files = _imageRepository.GetImages();
-            //foreach(var file in _imageRepository.GetImages())
-            //{
-            //    var content = file.ImageData ?? new byte[0] ;
-            //    files.Add(File(content, "image/jpeg"));
-            //    //files.Add(file.ImageData);
-            //    //using (Stream filee = System.IO.File.OpenWrite(@"C:\Users\Hamza Rahman\source\repos\TestImageApp\TestImageApp\ClientApp\here.jpeg"))
-            //    //{
-            //    //    filee.Write(file.ImageData, 0, file.ImageData.Length);
-            //    //}
-            //}
             return files;
         }
     }
